fe8002b9533e6f55fe0549868c87a8d5ae7312b0
AliFoxExamples <user-ali-sadykov@yandex.ru>
2021-03-11 18:27:00 +0300

    feat: add main algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split('');
+
+  let map = new Map (arr);
+
+  for (let i = 0; i < str.length-1; i++) {
+
+
+      if ( str[i+1] == map.get(str[i]) ) {
+
+          str.splice(i,2);
+
+          if (!str.length) {
+
+              return true;
+          }
+
+          i = -1;
+
+      }
+  }
+
+  return str.length? false: true;
 }
